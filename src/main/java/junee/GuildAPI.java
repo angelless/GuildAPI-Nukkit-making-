b31b72b3f60ae316150533b71ad80ef39b415d53
junee <junee1163@gmail.com>
2016-10-15 19:38:20 +0900

    Update GuildAPI.java

--- src/main/java/junee/GuildAPI.java
+++ src/main/java/junee/GuildAPI.java
@@ -2,15 +2,22 @@ package junee;
 
 import junee.Commands.*;
 
+import cn.nukkit.utils.Config;
+import cn.nukkit.plugin.PluginBase;
+
 class iGuild extends PluginBase {
+	
+	public Config config; 
 
-  public Config config;
-
-  public void onLoad(){
-  }
-  public void onEnable(){
-    this.getServer().getCommandMap().register("길드초대", new GuildInvite());
-    this.getServer().getCommandMap().register("길드매니져임명" new regGuildManager());
-    this.getServer().getCommandMap().register("길드탈퇴", new GuildLeave());
-  }
+	public void onLoad(){
+	}
+	public void onEnable(){
+    //커맨드 등록
+		this.getServer().getCommandMap().register("길드초대", new GuildInvite());
+		this.getServer().getCommandMap().register("부길드장", new regGuildManager());
+		this.getServer().getCommandMap().register("길드탈퇴", new GuildLeave());
+	}
+	
+	public void onDisable(){
+	}
 }
