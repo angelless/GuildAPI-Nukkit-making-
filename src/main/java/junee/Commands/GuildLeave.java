da37b70c1ff6d4a3add72b3e2692793be58c5a91
junee <junee1163@gmail.com>
2016-10-15 19:39:30 +0900

    Update GuildLeave.java

--- src/main/java/junee/Commands/GuildLeave.java
+++ src/main/java/junee/Commands/GuildLeave.java
@@ -1,7 +1,18 @@
 package junee.Commands;
 
-import cn.nukkit.command.Commands;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
 
-class GuildLeave extends Command{
+public class GuildLeave extends Command {
+	
+	public GuildLeave(){
+		super("길드탈퇴","현재 가입되어있는 길드에서 탈퇴합니다","/길드탈퇴");
+	}
+	
+	@Override
+	public boolean execute(CommandSender arg0, String arg1, String[] arg2) {
+		// TODO 자동 생성된 메소드 스텁
+		return false;
+	}
 
 }
