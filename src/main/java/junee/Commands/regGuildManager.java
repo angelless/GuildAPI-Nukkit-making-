687218176d50e4e9e3c5d9ab5b92f3ff8e571458
junee <junee1163@gmail.com>
2016-10-15 19:39:56 +0900

    Update regGuildManager.java

--- src/main/java/junee/Commands/regGuildManager.java
+++ src/main/java/junee/Commands/regGuildManager.java
@@ -1,7 +1,18 @@
 package junee.Commands;
 
 import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
 
-class regGuildManager extends Command{
-  
+public class regGuildManager extends Command{
+
+public regGuildManager(){
+  super("부길드장임명","부길드장를 임명/해임 합니다","/길드매니져임명 <닉네임>");
+}
+
+@Override
+public boolean execute(CommandSender arg0, String arg1, String[] arg2) {
+	// TODO 자동 생성된 메소드 스텁
+	return false;
+}
+
 }
