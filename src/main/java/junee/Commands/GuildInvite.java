03d987c5a57499607a15b52e80d7db58e31c030a
junee <junee1163@gmail.com>
2016-10-15 19:38:54 +0900

    Update GuildInvite.java

--- src/main/java/junee/Commands/GuildInvite.java
+++ src/main/java/junee/Commands/GuildInvite.java
@@ -1,7 +1,18 @@
 package junee.Commands;
 
-import cn.nukkit.command.Commands;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
 
-class GuildLeave extends Command{
+public class GuildInvite extends Command {
+	
+	public GuildInvite() {
+		super("길드초대","길드에 플레이어를 초대합니다","/길드초대 <플레이어>");
+	}
+
+	@Override
+	public boolean execute(CommandSender arg0, String arg1, String[] arg2) {
+		// TODO 자동 생성된 메소드 스텁
+		return false;
+	}
 
 }
